ec647c1c65cc9bab7bb24c37a168e88b9497e423
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-27 15:35:16 -0600

    commit tree 324afa394a9d9dc6fbe9082cc6e4f5f78be5224f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (function(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+})
+
+const Calculator = {};
+
+Calculator.add = function() {
+    return 1 + 3
+}
+Calculator.subtract = function() {
+    return 1 - 3
+}
+Calculator.multiply = function() {
+    return 1 * 3
+}
+Calculator.divide = function() {
+    return 10 / 5
+}
+
+function actionApplyer(integer, array) {
+    if (array.length < 1) {
+        return integer
+    } else {
+        return 4
+    }
+}
